dc42e0a358629227962aec04f10503f74bd0fddb
Alexandra <shestakova18@bk.ru>
2020-09-09 20:14:15 +0300

    examples with hooks

---
+++ src/use-effect.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+ 
+const App = () => {
+  const [value, setValue] = useState(0);
+  const [visible, setVisible] = useState(true);
+ 
+  if (visible) {
+    return (
+      <div>
+        <button
+          onClick={()=>setValue((v)=>v+1)}
+        >+
+        </button>
+ 
+        <button
+          onClick={()=>setVisible(false)}
+        >hide
+        </button>
+        <Notification />
+      </div>
+    )
+  }
+  else {
+    return <button onClick={()=>setVisible(true)}>show</button>
+  }
+};
+ 
+const HookCounter = ({value}) => {
+
+    useEffect(() => {
+        console.log('mount');
+        return () => console.log('unmount');
+        }, []);
+
+    useEffect(() => console.log('update'));
+
+    useEffect(() => () => console.log('unmount'), []);
+ 
+  return <p>{value}</p>;
+};
+ 
+const Notification = () => {
+
+    const [ visible, setVisible ] = useState(true);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setVisible(false), 2500);
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
+        <div>
+            {visible && <p>Hello</p>}
+        </div>
+    );
+};
+ 
+ReactDOM.render(<App />, document.getElementById('root'));
